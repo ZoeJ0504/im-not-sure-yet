d0134e609b2db0b0d826212333f1dd41d5586733
Zoe Mendez <zoemendez316@gmail.com>
2022-07-08 14:39:05 -0600

    Trying things out

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const h1 = document.createElement("h1")
+h1.textContent = "Zoe Mendez"
+document.querySelector("body").appendChild(h1)
 const h2 = document.createElement("h2")
 h2.textContent = "This content added by Javascript"
 document.querySelector("body").appendChild(h2)
