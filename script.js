c3e6f93480b3d9a4f029d261af8b346abc39c118
Zoe Mendez <zoemendez316@gmail.com>
2022-07-10 12:03:21 -0600

    added new pages and design

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const h1 = document.createElement("h1")
 h1.textContent = "Zoe Mendez"
+h1.id = "name"
 document.querySelector("body").appendChild(h1)
 const h2 = document.createElement("h2")
-h2.textContent = "This content added by Javascript"
+h2.textContent = "Welcome to Me!"
+h2.className = "topHeader"
 document.querySelector("body").appendChild(h2)
